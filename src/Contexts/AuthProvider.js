2ce83fdb1af7428e0755d0d3ad075d46555e28bc
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-02-27 12:35:05 +0600

    Updated email & pass login in Authprovider

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -1,10 +1,36 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged} from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
+    const googleProvider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const authInfo = [];
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Observer to catch the User 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+
+            return unsubscribe();
+        }
+    }, [auth]);
+
+    const authInfo = [
+        user,
+        createUser,
+    ];
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
